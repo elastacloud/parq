a481b7d2e9232327542eb0fdce769274638f645d
Andy Cross <andy@elastacloud.com>
2017-07-29 10:56:29 +0100

    add first commit

---
+++ src/parq/Display/Models/ConsoleFold.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parq.Display.Models
+{
+    class ConsoleFold
+    {
+      public long IndexStart { get; set; }
+      public long IndexEnd { get; set; }
+   }
+}
