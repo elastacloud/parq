f897853c456ccf94a668de4c8921c257ac65b525
Andy Cross <andy@elastacloud.com>
2017-07-31 13:38:35 +0100

    #12 take on board critique from chocolatey moderators

--- scripts/choco/parq/tools/chocolateyinstall.ps1
+++ scripts/choco/parq/tools/chocolateyinstall.ps1
@@ -1,8 +1,5 @@
 $toolsDirectory = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-Set-Location $toolsDirectory
-Add-Type -assembly "System.IO.Compression.FileSystem"
-    
+   
 $pathToArchive = [System.IO.Path]::Combine($toolsDirectory, "parqInstall.zip")
 Get-ChocolateyUnzip $pathToArchive $pwd
 Install-BinFile -Name parq -path "$toolsDirectory\\parq.exe"
